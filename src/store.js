7c5bdf931ceb9777cbcb95d77baed6efe19b6d88
Михаил Яранцев <mihail.yarantsev@travelline.ru>
2020-04-14 07:50:07 +0300

    add state

--- src/store.js
+++ src/store.js
@@ -4,7 +4,13 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+    state: {
+        user: {
+            id: '1',
+            name: 'Mixa'
+        },
+        categories: ['sustainability', 'nature', 'animal welfare', 'housing', 'education', 'food', 'community']
+    },
+    mutations: {},
+    actions: {}
 })
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create Event</h1>
+    <div>
+      User name: {{ userName }}<br>
+      Categories:
+      <ul>
+        <li v-for="category in categories">{{category}}</li>
+      </ul>
+    </div>
+  </div>
 </template>
+
+<script>
+  import { mapState } from 'vuex';
+
+  export default {
+      computed: {
+          ...mapState({
+              userName: state => state.user.name,
+              categories: 'categories', //эквивалентно state => state.categories.
+          })
+      }
+  }
+</script>
