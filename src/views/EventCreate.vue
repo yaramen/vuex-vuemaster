7db23f20edb83b6d4a163d93da06f0c220355b44
Михаил Яранцев <mihail.yarantsev@travelline.ru>
2020-04-15 07:32:46 +0300

    add redirect in action

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -76,6 +76,12 @@
             createEvent() {
                 this.$store.dispatch('createEvent', this.event)
                     .then(() => {
+                        this.$router.push({
+                            name: 'event-show',
+                            params: {
+                                id: this.event.id
+                            }
+                        });
                         this.event = this.createFreshEventObject()
                     })
                     .catch(() => {
